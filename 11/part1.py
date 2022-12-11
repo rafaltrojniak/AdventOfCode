beb39aff19af85899652342e65847e86f53d5006
Rafał Trójniak <git@trojniak.net>
2022-12-11 17:48:02 +0100

    day 11 part1 - solution

--- 11/part1.py
+++ 11/part1.py
@@ -4,6 +4,7 @@ import re
 
 class Monkey:
     id: int
+    work_counter: int
     items: list
     operation: str
     operation_operand: Union[str, int]
@@ -12,7 +13,7 @@ class Monkey:
 
     def __init__(self, stream):
         #Monkey 0:
-        self.id = map(int, re.findall(r"Monkey (\d+):", stream.readline())[0])
+        self.id = int(re.findall(r"Monkey (\d+):", stream.readline())[0])
         #  Starting items: 79, 98
         items = re.findall(r"Starting items: ([0-9, ]+)$", stream.readline())[0]
         self.items = list(map(int, items.split(', ')))
@@ -35,6 +36,37 @@ class Monkey:
            stream.readline())[0]),
         ]
         logging.info(f'targets: {self.target}')
+        self.work_counter = 0
+
+    def run(self, other_monkeys):
+        logging.info(f'Monkey {self.id}:')
+        for item in self.items:
+            self.work_counter+=1
+            logging.info(f'  Monkey inspects an item with a worry level of {item}.')
+            if self.operation_operand == 'old':
+                operation_operand = item
+            else:
+                operation_operand = int(self.operation_operand)
+            if self.operation == '*':
+                item *= operation_operand
+                logging.info(f'    Worry level is multiplied by {operation_operand} to {item}.')
+            elif self.operation == '+':
+                item += operation_operand
+                logging.info(f'    Worry level increases by {operation_operand} to {item}.')
+            else:
+                raise Exception('Unknown operation {self.operation}')
+            item =  int(item/3)
+            logging.info(f'    Monkey gets bored with item. Worry level is divided by 3 to {item}.')
+            if item % self.division_factor:
+                logging.info(f'    Current worry level is not divisible by {self.division_factor}.')
+                logging.info(f'    Item with worry level {item} is thrown to monkey {self.target[1]}.')
+                other_monkeys[self.target[1]].items.append(item)
+            else:
+                logging.info(f'    Current worry level is divisible by {self.division_factor}.')
+                logging.info(f'    Item with worry level {item} is thrown to monkey {self.target[0]}.')
+                other_monkeys[self.target[0]].items.append(item)
+
+        self.items = []
 
 
 def read_monkeys(stream):
@@ -45,9 +77,18 @@ def read_monkeys(stream):
             break
     return monkeys
 
+def run_round(monkeys):
+    for monkey in monkeys:
+        monkey.run(monkeys)
 
 def run(stream):
-    pass
+    monkeys = read_monkeys(stream)
+    for round_id in range(1, 20+1):
+        run_round(monkeys)
+    work = [monkey.work_counter for monkey in monkeys]
+    work.sort(reverse=True)
+    logging.info(work)
+    return work[0]*work[1]
 
 if __name__ == "__main__":
     with open('input.txt') as indata:
--- 11/test_part1.py
+++ 11/test_part1.py
@@ -1,5 +1,6 @@
 import pytest
 import part1
+import logging
 
 items_started = [
         [79, 98],
@@ -8,23 +9,110 @@ items_started = [
         [74],
     ]
 
-items_first_round =  [
-    [20, 23, 27, 26],
-    [2080, 25, 167, 207, 401, 1046],
-    [],
-    [],
-]
+items_after_rouns = {
+    1:[
+        [20, 23, 27, 26],
+        [2080, 25, 167, 207, 401, 1046],
+        [],
+        [],
+    ],
+    2:[
+        [ 695, 10, 71, 135, 350],
+        [ 43, 49, 58, 55, 362],
+        [],
+        [],
+    ],
+    3:[
+        [ 16, 18, 21, 20, 122],
+        [ 1468, 22, 150, 286, 739],
+        [ ],
+        [ ],
+    ],
+    4:[
+        [ 491, 9, 52, 97, 248, 34],
+        [ 39, 45, 43, 258],
+        [ ],
+        [ ],
+    ],
+    5:[
+        [ 15, 17, 16, 88, 1037],
+        [ 20, 110, 205, 524, 72],
+        [ ],
+        [ ],
+    ],
+    6:[
+        [ 8, 70, 176, 26, 34],
+        [ 481, 32, 36, 186, 2190],
+        [ ],
+        [ ],
+    ],
+    7:[
+        [ 162, 12, 14, 64, 732, 17],
+        [ 148, 372, 55, 72],
+        [ ],
+        [ ],
+    ],
+    8:[
+        [ 51, 126, 20, 26, 136],
+        [ 343, 26, 30, 1546, 36],
+        [ ],
+        [ ],
+    ],
+    9:[
+        [ 116, 10, 12, 517, 14],
+        [ 108, 267, 43, 55, 288],
+        [ ],
+        [ ],
+    ],
+    10:[
+        [ 91, 16, 20, 98],
+        [ 481, 245, 22, 26, 1092, 30],
+        [ ],
+        [ ],
+    ],
+    15:[
+        [ 83, 44, 8, 184, 9, 20, 26, 102],
+        [ 110, 36],
+        [ ],
+        [ ],
+    ],
+    20:[
+        [ 10, 12, 14, 26, 34],
+        [ 245, 93, 53, 199, 115],
+        [ ],
+        [ ],
+    ]
+}
 
-def test_read_items():
+work_count_after_rounds={
+    20: [ 101, 95, 7, 105]
+}
+
+
+def test_items_1_round():
     with open('test_input1.txt') as testin:
-        assert [list(monkey.items) for monkey in part1.read_monkeys(testin)] == items_started
+        monkeys = part1.read_monkeys(testin)
 
-def test_monkeys_after_first_round():
-    pass
+    assert [list(monkey.items) for monkey in monkeys ] == items_started
 
+    for round_id in range(1,21):
+        logging.info(f'Starting round {round_id}')
+        part1.run_round(monkeys)
+        if round_id in items_after_rouns:
+            assert [list(monkey.items) for monkey in monkeys ] ==\
+            items_after_rouns[round_id], \
+            f'asserting round {round_id}'
+
+        if round_id in work_count_after_rounds:
+            assert [monkey.work_counter for monkey in monkeys] ==\
+                work_count_after_rounds[round_id ]
 
 
 def test_test_input():
     with open('test_input1.txt') as testin:
         assert part1.run(testin) == 10605
 
+
+def test_real():
+    with open('input.txt') as testin:
+        assert part1.run(testin) == 76728
